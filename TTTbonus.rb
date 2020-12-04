eb12fca9abaef80ab2954ba19e5be0cb9040c51e
Rebecca M Thomas <72821399+rthomasbazin@users.noreply.github.com>
2020-12-03 17:28:04 -0700

    Add files via upload

---
+++ TTTbonus.rb
@@ -0,0 +1,135 @@
+
+
+INITIAL_MARKER = ' '
+PLAYER_MARKER = 'X'
+COMPUTER_MARKER = 'O'
+WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
+                [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # cols
+                [[1, 5, 9], [3, 5, 7]]              # diags
+
+def prompt(msg)
+  puts "=> #{msg}"
+end
+
+def display_board(brd)
+  system 'clear'
+  puts "You're an #{PLAYER_MARKER}. Computer is #{COMPUTER_MARKER}"
+  puts ""
+  puts "     |     |"
+  puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}"
+  puts "     |     |"
+  puts "-----+-----+-----"
+  puts "     |     |"
+  puts "  #{brd[4]}  |  #{brd[5]}  |  #{brd[6]}"
+  puts "     |     |"
+  puts "-----+-----+-----"
+  puts "     |     |"
+  puts "  #{brd[7]}  |  #{brd[8]}  |  #{brd[9]}"
+  puts "     |     |"
+  puts ""
+end
+
+def joinor(arr, delimiter=', ', word='or')
+  case arr.size
+  when 0 then ''
+  when 1 then arr.first
+  when 2 then arr.join(" #{word} ")
+  else
+    arr[-1] = "#{word} #{arr.last}"
+    arr.join(delimiter)
+  end
+end
+
+def initialize_board
+  new_board = {}
+  (1..9).each {|num| new_board[num] = INITIAL_MARKER}
+  new_board
+end
+
+def empty_squares(brd)
+  brd.keys.select{|num| brd[num] == INITIAL_MARKER}
+end
+
+def player_places_piece!(brd)
+  square = ''
+  loop do
+    prompt "Choose a position to place a piece: #{joinor(empty_squares(brd))}:"
+    square = gets.chomp.to_i
+    break if empty_squares(brd).include?(square) 
+    prompt "Sorry, that's not a valid choice."
+  end
+  brd[square] = PLAYER_MARKER
+end
+
+def find_at_risk_square(line, board_full)
+  if board.values_at(*line).count(PLAYER_MARKER) == 2
+    board.select{ |k, v| line.include?(k) && v == INITIAL_MARKER }.keys.first
+  else
+    nil
+  end
+end
+
+def computer_places_piece!(brd)
+  square = nil
+  WINNING_LINES.each do |line|
+    square = find_at_risk_square(line, brd)
+    break if square
+  end
+
+  if !square
+    square = empty_squares(brd).sample
+  end
+
+  brd[square] = COMPUTER_MARKER
+end
+
+def board_full?(brd)
+  empty_squares(brd).empty?
+end
+
+def someone_won?(brd)
+  !!detect_winner(brd)
+end
+
+def detect_winner(brd)
+  WINNING_LINES.each do |line|
+    if brd[line[0]] == PLAYER_MARKER && 
+       brd[line[1]] == PLAYER_MARKER &&
+       brd[line[2]] == PLAYER_MARKER
+      return 'Player'
+    elsif brd[line[0]] == COMPUTER_MARKER && 
+          brd[line[1]] == COMPUTER_MARKER &&
+          brd[line[2]] == COMPUTER_MARKER
+     return 'Computer'
+    end
+  end
+  nil
+end
+
+loop do
+board = initialize_board
+
+  loop do
+    display_board(board)
+
+    player_places_piece!(board)
+    break if someone_won?(board) || board_full?(board)
+
+    computer_places_piece!(board)
+    break if someone_won?(board) || board_full?(board)
+  end
+
+  display_board(board)
+
+  if someone_won?(board)
+    prompt "#{detect_winner(board)} won!"
+  else
+    prompt "It's a tie!"
+  end
+
+  prompt "Play again? (y or n)"
+  answer = gets.chomp
+  break unless answer.downcase.start_with?('y')
+end
+
+prompt "Thanks for playing Tic Tac Toe! Goodbye!"
